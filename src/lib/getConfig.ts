8bca7963033ae8e17f8021519082effb2250d41a
Vordgi <sasha2822222@gmail.com>
2023-12-21 20:32:52 +0400

    nt-25 add advanced caching option

--- src/lib/getConfig.ts
+++ src/lib/getConfig.ts
@@ -2,20 +2,31 @@ import fs from 'fs';
 import path from 'path';
 import { pathToFileURL } from 'url';
 import { type Config } from '../types';
+import DataLoader from './DataLoader';
+
+type ModifiedConfig = { dataLoader: DataLoader }
 
 const CONFIG_PATH = path.join(process.cwd(), 'next-translation.js');
-const configRef = { current: null as Config | null };
+const configRef = { current: null as ModifiedConfig | null };
 // Crutch bypass of conversion by the assembler to require
 const dynamicImport = new Function('p', 'return import(p)');
 
-const getConfig = async (): Promise<Config> => {
+const getConfig = async (): Promise<ModifiedConfig> => {
   if (configRef.current) return configRef.current;
 
   try {
     if (fs.existsSync(CONFIG_PATH)) {
-      const config = await dynamicImport(pathToFileURL(CONFIG_PATH).href);
-      configRef.current = config.default;
-      return config.default;
+      const config: { default: Config } = await dynamicImport(pathToFileURL(CONFIG_PATH).href);
+      const { loaderProvider, unstable_advancedLoader } = config.default;
+
+      if (!loaderProvider) {
+        throw new Error(`Can't find loaderProvider - https://github.com/vordgi/next-translation#configuration`);
+      }
+
+      configRef.current = {
+        dataLoader: new DataLoader({ loaderProvider, unstable_advancedLoader })
+      };
+      return configRef.current;
     }
   } catch {
     //
