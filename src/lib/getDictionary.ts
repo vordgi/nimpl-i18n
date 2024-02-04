453ee36c0e089622dac5338466332a979db0ad57
Vordgi <sasha2822222@gmail.com>
2024-02-04 22:23:53 +0400

    nt-35 finalize getdictionary

--- src/lib/getDictionary.ts
+++ src/lib/getDictionary.ts
@@ -1,7 +1,8 @@
 import { type Translates } from '../types';
 
 const getDictionary = async (lang: string): Promise<Translates> => {
-  const dataResp = await (globalThis as any)._nextOriginalFetch(`http://localhost:${process.env.NEXT_TRANSLATION_CACHE_PORT}/?secret=${process.env.NEXT_TRANSLATION_CACHE_SECRET}&lang=${lang}`, { cache: 'no-cache' });
+  const origFetch = (globalThis as any)._nextOriginalFetch || fetch;
+  const dataResp = await origFetch(`http://localhost:${process.env.NEXT_TRANSLATION_CACHE_PORT}/?secret=${process.env.NEXT_TRANSLATION_CACHE_SECRET}&lang=${lang}`, { cache: 'no-cache' });
   const data = await dataResp.json();
 
   return data;
