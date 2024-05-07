4d2a7c69b09141e889f6fe75b1f1484fa0158050
Alexander Savelyev <91429106+vordgi@users.noreply.github.com>
2024-05-07 23:21:52 +0400

    nt-87 improve get-config errors (#88)
    nt-87 improve get-config errors

--- package/src/configuration/getConfig.ts
+++ package/src/configuration/getConfig.ts
@@ -8,35 +8,34 @@ const CONFIG_PATH = path.join(process.cwd(), "nimpl-i18n.js");
 const dynamicImport = new Function("p", "return import(p)");
 
 const getConfig = async (): Promise<Config> => {
-    try {
-        if (fs.existsSync(CONFIG_PATH)) {
-            const config: { default: Config } = await dynamicImport(pathToFileURL(CONFIG_PATH).href);
-            const { load, getLanguage, languages } = config.default;
+    if (fs.existsSync(CONFIG_PATH)) {
+        const config: { default: Config } = await dynamicImport(pathToFileURL(CONFIG_PATH).href);
+        const { load, getLanguage, languages } = config.default;
 
-            if (!load) {
-                throw new Error(
-                    `Can't find load method in configuration file - https://github.com/vordgi/nimpl-i18n#configuration`,
-                );
-            }
-
-            if (!languages) {
-                throw new Error(
-                    `Can't find languages list in configuration file - https://github.com/vordgi/nimpl-i18n#configuration`,
-                );
-            }
-
-            if (!getLanguage) {
-                throw new Error(
-                    `Can't find getLanguage method in configuration file - https://github.com/vordgi/nimpl-i18n#configuration`,
-                );
-            }
-
-            return config.default;
+        if (!load) {
+            throw new Error(
+                `Can't find "load" method in configuration file - https://github.com/vordgi/nimpl-i18n#configuration`,
+            );
         }
-    } catch (err) {
-        console.error(err);
+
+        if (!languages) {
+            throw new Error(
+                `Can't find "languages" list in configuration file - https://github.com/vordgi/nimpl-i18n#configuration`,
+            );
+        }
+
+        if (!getLanguage) {
+            throw new Error(
+                `Can't find "getLanguage" method in configuration file - https://github.com/vordgi/nimpl-i18n#configuration`,
+            );
+        }
+
+        return config.default;
+    } else {
+        throw new Error(
+            `Can't load config from "${CONFIG_PATH}". Read more - https://github.com/vordgi/nimpl-i18n#configuration`,
+        );
     }
-    throw new Error("Can't load config - https://github.com/vordgi/nimpl-i18n#configuration");
 };
 
 export default getConfig;
