88cb5c1f6ae2e001f6b4a924a93681a47c97e2aa
Vordgi <sasha2822222@gmail.com>
2024-05-11 19:14:42 +0400

    nt-93 simplify load return type

--- package/src/configuration/types.ts
+++ package/src/configuration/types.ts
@@ -1,7 +1,9 @@
+import { type Translates } from "../types";
+
 export type Meta = { lastUpdated: number; isRevalidated?: boolean } & Record<string, unknown>;
 
 export type Config = {
-    load(key: string, meta?: Meta): Promise<{ data: unknown; meta: Meta }>;
+    load(key: string, meta?: Meta): Promise<Translates>;
     getLanguage(options: { pathname: string | null; params: { [key: string]: string | string[] } }): Promise<string>;
     languages: string[];
 };
